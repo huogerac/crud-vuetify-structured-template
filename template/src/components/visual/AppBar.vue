8bb4af5da1f57b0632491f40bb799ce0e0be986b
Roger Camargo <huogerac@gmail.com>
2021-08-24 15:39:27 -0300

    Change to get the page title from the project name

--- template/src/components/visual/AppBar.vue
+++ template/src/components/visual/AppBar.vue
@@ -2,7 +2,7 @@
   <v-toolbar dark prominent :src="background">
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    <v-toolbar-title>Tasks</v-toolbar-title>
+    <v-toolbar-title>{{name}}</v-toolbar-title>
 
     <v-spacer></v-spacer>
   </v-toolbar>
